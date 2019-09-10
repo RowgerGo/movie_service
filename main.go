35f30b584b595fb79568e73f6709401cf36a1633
rowger <php_echo@163.com>
2019-09-10 12:01:31 +0800

    修改端口

--- main.go
+++ main.go
@@ -35,8 +35,8 @@ func main() {
 	go handleMessages()
 
 	// Start the server on localhost port 8000 and log any errors
-	log.Println("http server started on :8000")
-	err := http.ListenAndServe(":8000", nil)
+	log.Println("http server started on :8226")
+	err := http.ListenAndServe(":8226", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
